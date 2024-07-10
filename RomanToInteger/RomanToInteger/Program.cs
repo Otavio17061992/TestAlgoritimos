918ef565062c5449710e6d9de97d76d650ff2878
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2024-07-10 02:56:05 +0000

    Exercicio Converter Roman para Decimal

---
+++ RomanToInteger/RomanToInteger/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConvertInteger("LVIII");
+    }
+
+
+    private static int ConvertInteger(string s)
+    {
+        int resp = 0, num =0;
+
+        for(int i = s.Length-1; i >= 0; i--)
+        {
+            switch(s[i])
+            {
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+        }
+
+        if(4*resp < num)
+        {
+            resp -= num;
+        }
+        else{
+            resp += num;
+        }
+
+        return resp;
+    }
+}
